1ef321860b6852c13fddf45ada2ebea502c12542
Donly Chan <donly@magicalboy.com>
2012-07-21 19:10:10 +0800

    Initial Commit

---
+++ LanguageDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LanguageDemo
+//
+//  Created by Donly Chan on 12-7-21.
+//  Copyright (c) 2012年 MAGICALBOY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LanguageDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LanguageDemo
+//
+//  Created by Donly Chan on 12-7-21.
+//  Copyright (c) 2012年 MAGICALBOY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
